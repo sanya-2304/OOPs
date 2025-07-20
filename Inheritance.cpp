e60ef7b62a075d1296f79f35face5ad597a8d97f
Sanya Doda <sanyaadoda@gmail.com>
2025-07-20 20:40:03 +0530

    Inheritance done right

---
+++ Inheritance.cpp
@@ -0,0 +1,71 @@
+// 🌟 Scenario: Smart Home Devices
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class SmartDevice{
+    protected:
+    int deviceId;
+    bool isOn=false;
+    public:
+        void turnOn(){
+            if(isOn){
+                cout<<"Device is on."<<endl;
+            }else {
+                 isOn=true;
+                cout<<"Device was off---------Now device is on."<<endl;
+            }
+        }
+        void turnOff(){
+            if(!isOn){
+                cout<<"Device is off."<<endl;
+            }else {
+                 isOn=false;
+                cout<<"Device was on---------Now device is off."<<endl;
+            }
+        }
+};
+
+class SmartLight: public SmartDevice{
+    private:
+    int brightness;
+    public: 
+     SmartLight(int val, int id){
+        brightness=val;
+        deviceId=id;
+    }
+    void details(){
+        cout<<"SmartLights-> deviceId--"<<deviceId<<" isOn--"<<isOn<<" brightness--"<<brightness<<endl;
+    }
+};
+class SmartFan: private SmartDevice{
+     private:
+    int speed;
+    public: 
+     SmartFan(int val, int id){
+        speed=val;
+        deviceId=id;
+    }
+    void setOn(){
+        isOn=true;
+    }
+     void details(){
+        cout<<"SmartFan-> deviceId--"<<deviceId<<" isOn--"<<isOn<<" speed--"<<speed<<endl;
+    }
+};
+
+int main(){
+    // SmartLight sl=new SmartLight(100);
+    // SmartFan sf=new SmartFan(42);
+    SmartFan sf(42, 1);
+    SmartLight sl(100, 2);
+    sl.details();
+      sl.turnOn();
+       sl.details();
+    sf.details();
+    sl.turnOff();
+    sl.details();
+    sf.setOn();
+    sf.details();
+    return 0;
+}
